c2bcecd94950e778be60f8833eb21cf249b2b725
Ewout ter Haar <ewoutterhaar@gmail.com>
2012-07-20 19:40:46 -0300

    Gráficos

--- enem.py
+++ enem.py
@@ -13,8 +13,8 @@ import statsmodels.api as sm
 
 #DATAFILE = '/home/ewout/enem/Microdados ENEM 2010/Dados Enem 2010/DADOS_ENEM_2010.txt'
 #DICFILE =  '/home/ewout/enem/Microdados ENEM 2010/Input_SAS/INPUT_SAS_ENEM_2010.SAS'
-#CSVFILE =  '/home/ewout/enem/Microdados ENEM 2009/Dados Enem 2009/DADOS_ENEM_2009.csv'
-CSVFILE =  '/home/ewout/enem/Microdados ENEM 2010/Dados Enem 2010/DADOS_ENEM_2010.csv'
+CSVFILE2009 =  '/home/ewout/enem/Microdados ENEM 2009/Dados Enem 2009/DADOS_ENEM_2009.csv'
+CSVFILE2010 =  '/home/ewout/enem/Microdados ENEM 2010/Dados Enem 2010/DADOS_ENEM_2010.csv'
 
 #gabarito2010 = {89:'BACEADCAECABDDACBABEBDEDAEECDBCDBDEEABDACEDDC',
 #                90:'ACAEBCCEADADDBACBEBABDEEADEDDBCBCDDBAEEACDDEC',
@@ -73,29 +73,44 @@ def resvec2(df,rescol='TX_RESPOSTAS_CN'):
     return df
 
 
-def itemfbar(acertos,order=True,fig=None,ax=None):
+def itemfbar(acertos,acertos2,order=True,fig=None,ax=None):
     ''
     itemstats, teststats = stats(acertos)
     itemf = itemstats['itemf']
+
+    itemstats2, teststats2 = stats(acertos2)
+    itemf2 = itemstats2['itemf']
+
+
     if order:
         itemfdf = pandas.DataFrame(itemstats['itemf'],index=range(1,len(itemf)+1))
         itemfdf = itemfdf.sort(columns=0)
+        itemfdf2 = pandas.DataFrame(itemstats2['itemf'],index=range(1,len(itemf2)+1))
+        itemfdf2 = itemfdf2.sort(columns=0)
+
 
     if not fig:
         fig = plt.figure()
     if not ax:
         ax = fig.add_subplot(111)
-        ax.set_title(u"Fração dos alunos que acertaram a questão")
-    width = 0.8
+        ax.set_title(u"Número do item em ordem de dificuldade")
+    width = 0.5
     if order:
-        ax.bar(np.arange(1,len(itemf)+1),itemfdf[0],width=width,align='center')
+        ax.bar(np.arange(1,len(itemf)+1),itemfdf[0],width=width,align='center',color='b',label="ENEM 2010")
+        ax.bar(np.arange(1+width,len(itemf)+1+width),itemfdf2[0],width=width,align='center',color="g",label="ENEM 2009")
     else:
         ax.bar(np.arange(1,len(itemf)+1),itemf,width=width,align='center')
 
-    ax.plot([0,len(itemf)+1],[0.2,0.2],'b',label="Chute",linewidth=2)
-    ax.plot([0,len(itemf)+1],[itemf.mean(),itemf.mean()],'k',label=u"Média",linewidth=2)
-    ax.legend()
-    ax.set_xlabel(u"Questão")
+#    ax.plot([0,len(itemf)+1],[0.2,0.2],'b',label="Chute",linewidth=2)
+#    ax.plot([0,len(itemf)+1],[itemf.mean(),itemf.mean()],'k',label=u"Média",linewidth=2)
+    m10 = itemf.mean()
+    m9 = itemf2.mean()
+    ax.annotate(u"Médias",xy=(0,m10),xytext=(10,m10),color='k',arrowprops=dict(facecolor='b',width=2,shrink=0.05))
+    ax.annotate("",xy=(0,m9),xytext=(10,m9),color='g',arrowprops=dict(facecolor='g',width=2,shrink=0.05))
+    ax.annotate("Chute",xy=(0,0.2),xytext=(10,0.25),color='k',arrowprops=dict(facecolor='grey',width=1,shrink=0.05))
+    ax.legend(loc=2)
+    ax.set_ylabel(u"Fração dos alunos que acertaram a questão")
+    ax.set_xlabel(u"Item")
     ax.set_ylim(0,1)
     ax.set_xlim(0,len(itemf)+1)
     if order:
@@ -104,14 +119,20 @@ def itemfbar(acertos,order=True,fig=None,ax=None):
         ax.set_xticks([1,5,10,15,20,25,30,35,40,45])
     return fig,ax
 
-def idbar(acertos,order=True,fig=None,ax=None):
+def idbar(acertos,acertos2,order=True,fig=None,ax=None):
     ''
     itemstats, teststats = stats(acertos)
+    itemstats2, teststats2 = stats(acertos2)
+
     #id50 = itemstats['id50']
     id27 = itemstats['id27']
+    id279 = itemstats2['id27']
     if order:
         iddf = pandas.DataFrame(id27,index=range(1,len(id27)+1))
         iddf = iddf.sort(columns=0)
+        iddf9 = pandas.DataFrame(id279,index=range(1,len(id279)+1))
+        iddf9 = iddf9.sort(columns=0)
+
 
 
     if not fig:
@@ -119,19 +140,21 @@ def idbar(acertos,order=True,fig=None,ax=None):
     if not ax:
         ax = fig.add_subplot(111)
         ax.set_title(u"Índice de Discriminação")
-    width = 0.8
+    width = 0.5
     if order:
-        ax.bar(np.arange(1,len(id27)+1),iddf[0],width=width,align='center',color='b',label=u'diferença de acertos entre os piores e melhores 27%')
+        ax.bar(np.arange(1,len(id27)+1),iddf[0],width=width,align='center',color='b',label=u'2010')
+        ax.bar(np.arange(1+width,len(id279)+1+width),iddf9[0],width=width,align='center',color='g',label="2009")
     else:
         ax.bar(np.arange(1,len(id27)+1),id27,width=width,align='center',color='b',label=u'diferença de acertos entre os piores e melhores 27%')
-    ax.set_xlabel(u"Questão")
-    ax.set_ylim(-0.05,1)
+    #ax.text(u'diferença de acertos entre os piores e melhores 27%')
+    ax.legend(loc="upper left")
+    ax.set_xlabel(u"Itens em ordem de discriminação")
+    ax.set_ylim(-0.05,0.4)
     ax.set_xlim(0,len(id27)+1)
     if order:
         ax.set_xticks([])
     else:
         ax.set_xticks([1,5,10,15,20,25,30,35,40,45])
-    ax.legend()
     return fig,ax
 
 
@@ -158,7 +181,7 @@ def gradebar(df,qn,fig=None,ax=None):
     ax.set_xticks(x+0.5*width)
     ax.set_xticklabels(labels)
     ax.text(0.02,0.8,'Q'+str(qn),transform = ax.transAxes)
-    return ax
+    return fig, ax
 
 def gradegrid(df,ncols=5,nrows=9,fig=None):
     ''
@@ -261,7 +284,12 @@ def iccfitgraph(df,acertos,fig=None):
 
 def csv2df(idprov=89,tipprov='CN',sexo=None, raca=None):
     'Import enem csv to dataframe, clean it up.'
-    csvfile = CSVFILE
+    if idprov in range(49,85):
+        csvfile = CSVFILE2009
+    elif idprov in range(89,117):
+        csvfile = CSVFILE2010
+    else:
+        raise Exception("ID da Prova errada!")
     df = pandas.read_table(csvfile)
     print "number of rows in df before filters:", len(df)
     df = df[df['IN_PRESENCA_'+tipprov] == 1]
@@ -289,9 +317,9 @@ def csv2df(idprov=89,tipprov='CN',sexo=None, raca=None):
 
 def iccgriddif(idprov=89,tipprov='CN',ncols=5,nrows=9):
     ''
-    df, itemstats, teststats, acertos = csv2df(idprov,tipprov)
-    dfm, itemstatsm, teststatsm, acertosm = csv2df(idprov,tipprov,sexo='M')
-    dff, itemstatsf, teststatsf, acertosf = csv2df(idprov,tipprov,sexo='F')
+    df, itemstats, teststats, acertos, acertosn = csv2df(idprov,tipprov)
+    dfm, itemstatsm, teststatsm, acertosm, acertosmn = csv2df(idprov,tipprov,sexo='M')
+    dff, itemstatsf, teststatsf, acertosf, acertosfn = csv2df(idprov,tipprov,sexo='F')
     #dfm, itemstatsm, teststatsm, acertosm = csv2df(idprov,tipprov,raca='1')
     #dff, itemstatsf, teststatsf, acertosf = csv2df(idprov,tipprov,raca='2')
     fig = plt.figure()
@@ -306,6 +334,19 @@ def iccgriddif(idprov=89,tipprov='CN',ncols=5,nrows=9):
     fig.subplots_adjust(left=0.1,right=0.95,bottom=0.05,top=0.9,wspace=0.4,hspace=0.4)
     return fig
 
+def generate_graphs(graphs = "all"):
+    ''
+    df, itemstats, teststats, acertos, acertosn = csv2df(idprov=89,tipprov='CN')
+    df9, itemstats9, teststats9, acertos9, acertosn9 = csv2df(idprov=49,tipprov='CN')
+
+
+    fig, ax = itemfbar(acertos,acertos9)
+    fig.savefig('figs/itemfbar.png')
+    # bar graph da questão 27 de 2010
+    fig, ax = gradebar(df,21)
+    fig.savefig('figs/gradebar-27-2010.png')
+    
+    
 if __name__ == '__main__':
-    df, itemstats, teststats, acertos = csv2df()
+    pass
     
